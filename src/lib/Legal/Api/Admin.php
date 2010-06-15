cdc027fdc3042170af3d73ecb568d8cadf6aef76
Drak <drak@zikula.org>
2010-06-15 15:53:45 +0000

    Restructuring
    git-svn-id: file:///home/trac/svn/legal/trunk@9 75f03c3e-4248-4141-b377-6a686ccf1b6f

---
+++ src/lib/Legal/Api/Admin.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ */
+
+class Legal_Api_Admin extends Zikula_Api
+{
+
+    /**
+     * reset the agreement to the terms of use for a speial group of users
+     *
+     * @param        gid   (int) the group id, -1=none, 0=all groups
+     * @return       output       The main module admin page.
+     */
+    public function resetagreement($args)
+    {
+        // Security check
+        if (!SecurityUtil::checkPermission('legal::', '::', ACCESS_ADMIN)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        if (!isset($args['gid']) || $args['gid'] == -1) {
+            return false;
+        }
+
+        // Get database setup
+        $pntable = System::dbGetTables();
+        $userscolumn = $pntable['users_column'];
+
+        if ($args['gid']==0) {
+            //all users
+            // creative usage of DBUtil
+            $object = array('activated' => 2);
+            $where = "WHERE $userscolumn[uid] NOT IN (1,2)";
+            DBUtil::updateObject($object, 'users', $where, 'uid');
+        } else {
+            // single group
+
+            // get the group incl members
+            $grp = ModUtil::apiFunc('Groups', 'user', 'get', array('gid' => $args['gid']));
+            if ($grp==false) {
+                return false;
+            }
+
+            // remove anonymous from members array
+            if (array_key_exists(1, $grp['members'])) {
+                unset($grp['members'][1]);
+            }
+
+            // remove admin from members array
+            if (array_key_exists(2, $grp['members'])) {
+                unset($grp['members'][2]);
+            }
+
+            // return if group is empty
+            if (count($grp['members'])==0) {
+                return false;
+            }
+            $members = '(' . implode(array_keys($grp['members']), ',') . ')';
+
+            // creative usage of DBUtil
+            $object = array('activated' => 2);
+            $where = "WHERE $userscolumn[uid] IN $members";
+            DBUtil::updateObject($object, 'users', $where, 'uid');
+        }
+        return true;
+    }
+}
---
+++ src/lib/Legal/User.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ */
+
+class Legal_User extends Zikula_Controller
+{
+
+    /**
+     * Legal Module main user function
+     * @return string HTML output string
+     */
+    public function main()
+    {
+        // Security check
+        if (!SecurityUtil::checkPermission('legal::', '::', ACCESS_OVERVIEW)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        // Create output object
+        $renderer = Renderer::getInstance('legal');
+
+        return $renderer->fetch('legal_user_main.htm');
+    }
+
+    /**
+     * Display Terms of Use
+     * @return string HTML output string
+     */
+    public function termsofuse()
+    {
+        // Security check
+        if (!SecurityUtil::checkPermission('legal::termsofuse', '::', ACCESS_OVERVIEW)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        // check the option is active
+        if (!ModUtil::getVar('legal', 'termsofuse')) {
+            return LogUtil::registerError($this->__("'Terms of use' not activated."));
+        }
+
+        // Create output object
+        $renderer = Renderer::getInstance('legal');
+
+        // get the current users language
+        $lang = ZLanguage::transformFS(ZLanguage::getLanguageCode());
+
+        // work out the template path
+        if ($renderer->template_exists($lang.'/legal_user_termsofuse.htm')) {
+            $template = $lang.'/legal_user_termsofuse.htm';
+        } else {
+            $template = 'en/legal_user_termsofuse.htm';
+        }
+
+        // check out if the contents are cached.
+        // If this is the case, we do not need to make DB queries.
+        if ($renderer->is_cached($template)) {
+            return $renderer->fetch($template);
+        }
+
+        return $renderer->fetch($template);
+    }
+
+    /**
+     * Display Privacy Policy
+     * @return string HTML output string
+     */
+    public function privacy()
+    {
+        // Security check
+        if (!SecurityUtil::checkPermission('legal::privacy', '::', ACCESS_OVERVIEW)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        // check the option is active
+        if (!ModUtil::getVar('legal', 'privacypolicy')) {
+            return LogUtil::registerError($this->__("'Privacy policy' not activated."));
+        }
+
+        // Create output object
+        $renderer = Renderer::getInstance('legal');
+
+        // get the current users language
+        $lang = ZLanguage::transformFS(ZLanguage::getLanguageCode());
+
+        // work out the template path
+        if ($renderer->template_exists($lang.'/legal_user_privacy.htm')) {
+            $template = $lang.'/legal_user_privacy.htm';
+        } else {
+            $template = 'en/legal_user_privacy.htm';
+        }
+
+        // check out if the contents are cached.
+        // If this is the case, we do not need to make DB queries.
+        if ($renderer->is_cached($template)) {
+            return $renderer->fetch($template);
+        }
+
+        return $renderer->fetch($template);
+    }
+
+    /**
+     * Display Accessibility statement
+     * @return string HTML output string
+     */
+    public function accessibilitystatement()
+    {
+        // Security check
+        if (!SecurityUtil::checkPermission('legal::accessibilitystatement', '::', ACCESS_OVERVIEW)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        // check the option is active
+        if (!ModUtil::getVar('legal', 'accessibilitystatement')) {
+            return LogUtil::registerError($this->__("'Accessibility statement' not activated."));
+        }
+
+        // Create output object
+        $renderer = Renderer::getInstance('legal');
+
+        // get the current users language
+        $lang = ZLanguage::transformFS(ZLanguage::getLanguageCode());
+
+        // work out the template path
+        if ($renderer->template_exists($lang.'/legal_user_accessibilitystatement.htm')) {
+            $template = $lang.'/legal_user_accessibilitystatement.htm';
+        } else {
+            $template = 'en/legal_user_accessibilitystatement.htm';
+        }
+
+        // check out if the contents are cached.
+        // If this is the case, we do not need to make DB queries.
+        if ($renderer->is_cached($template)) {
+            return $renderer->fetch($template);
+        }
+
+        return $renderer->fetch($template);
+    }
+}
