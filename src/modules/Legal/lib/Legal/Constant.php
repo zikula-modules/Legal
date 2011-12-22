ab0ddd5d5803351e84621ae52a4f5300f1e9ba0a
Robert Burkhead <rmburkhead@gmail.com>
2011-12-22 03:25:06 -0700

    Refactor: coding standards and 1.3 modernization. Refs #10

--- src/modules/Legal/lib/Legal/Constant.php
+++ src/modules/Legal/lib/Legal/Constant.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Copyright 2011 Zikula Foundation.
- * 
+ * Copyright (c) 2001-2012 Zikula Foundation
+ *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
- * 
- * @license GNU/LGPLv3 (or at your option, any later version).
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html GNU/LGPLv3 (or at your option any later version).
  * @package Legal
- * 
+ *
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
