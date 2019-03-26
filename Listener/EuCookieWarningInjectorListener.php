9be5b415985d919095df618f89fda7e04fea293a
Guite <info@guite.de>
2019-03-26 14:19:30 +0100

    cs

--- Listener/EuCookieWarningInjectorListener.php
+++ Listener/EuCookieWarningInjectorListener.php
@@ -15,7 +15,6 @@ namespace Zikula\LegalModule\Listener;
 
 use Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
